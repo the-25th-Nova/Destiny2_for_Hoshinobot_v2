2d5e33da6ee88d445e81a2734b16969695e06b38
NovaNoir <68760718+the-25th-Nova@users.noreply.github.com>
2020-08-01 23:16:29 +0800

    Update findxur.py

--- findxur.py
+++ findxur.py
@@ -9,11 +9,45 @@ print('Done!')
 #import datetime 
 from datetime import*
 import calendar, math
+import requests
+from bs4 import BeautifulSoup
+
+headers={'User-Agent':'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.137 Safari/537.36 LBBROWSER'}
+def get_soup():
+	url = 'https://xur.wiki/'
+	req = requests.get(url, headers=headers)
+	soup = BeautifulSoup(req.text, 'lxml')
+	return soup
+	
+def get_xur_item():
+	soup = get_soup()
+	xml = soup.find_all('h6', class_='hero_item_name')
+	item = []
+	for i in xml:
+		item.append(i.string)
+	return item
+
+def get_xur_pos():
+	soup = get_soup()
+	item = []
+	xml = soup.find_all('h1', class_='location_name')
+	#print(xml)
+	item.append(xml[0].text)
+	xml = soup.find('p', class_='location-text-here').contents[0].text
+	item.append(xml)
+	xml = soup.find_all('div', class_='location-map lazyload')
+	item.append(xml[0].get('data-src'))
+	return item
+
+#老九时间
 
 def find_xur():
 	if is_xur_arrived():
 		#print('Xur was arrived')
 		msg = '老九已经到了'
+		item = get_xur_pos()
+		for i in item:
+			msg += f'\n{i}'
 	else:
 		#print('Xur is not here')
 		time = xur_dis()
@@ -23,7 +57,7 @@ def find_xur():
 def is_xur_arrived():
 	today = datetime.today().weekday()
 	#6 0 1 2 3 4 5
-	xur_days = [0,1,5,6]
+	xur_days = [0,1,6]
 	#if today == (5 or 6 or 0 or 1 or 3):
 	if today in xur_days:
 		return True
@@ -32,6 +66,11 @@ def is_xur_arrived():
 			return False
 		else:
 			return True
+	elif today == 5:
+		if datetime.now().hour >= 1:
+			return True
+		else:
+			return False
 	else:
 		return False
 	
